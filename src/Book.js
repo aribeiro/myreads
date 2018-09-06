bd23d09a542d6679b325e36159c1d25d209130fa
Alfredo Ribeiro <me@alfredoribeiro.com>
2018-09-06 10:14:42 -0300

    feat: Replace Hard coded Books from Books from BooksAPI

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Book = (props) => {
+  const { book } = props
   return ( 
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+        <div className="book-cover" style={{ width: 128, 
+            height: 193, 
+            backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
         <div className="book-shelf-changer">
           <select>
             <option value="move" disabled>Move to...</option>
@@ -15,8 +18,8 @@ const Book = (props) => {
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.author && book.author.join(", ")}</div>
     </div>
   )
 
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,8 +7,8 @@ const BookShelf = (props) => {
       <h2 className="bookshelf-title">{props.title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {props.books.map( book => ( 
-            <li> 
+          {props.books.map( (book) => ( 
+            <li key={book.id} > 
               <Book book={book} /> 
             </li>
           ))}
