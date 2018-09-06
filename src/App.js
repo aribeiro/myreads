86bea743920d2d1e52706b91b144708cb7169840
Alfredo Ribeiro <me@alfredoribeiro.com>
2018-09-06 11:47:36 -0300

    feat: Extract ListBooks from App.js in its own Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks'
-import BookShelf from './BookShelf'
+import ListBooks from './ListBooks'
 
 import './App.css'
 
@@ -31,27 +31,14 @@ class BooksApp extends Component {
 
   render() {
     const { books } = this.state
-    const currentlyReading = books.filter(book => book.shelf === "currentlyReading")
-    const wantToRead = books.filter(book => book.shelf === "wantToRead")
-    const read = books.filter(book => book.shelf === "read")
     return (
       <div className="app">
         <Route exact path="/" render={ () =>  (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf title="Currenly Reading" books={currentlyReading} onChangeShelf={this.changeShelf.bind(this)} />
-                <BookShelf title="Want to Read" books={wantToRead} onChangeShelf={this.changeShelf.bind(this)} />
-                <BookShelf title="Read" books={read} onChangeShelf={this.changeShelf.bind(this)} />
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
+          <ListBooks
+            books={books} 
+            onChangeShelf={this.changeShelf.bind(this)} 
+          />
+
         )} />
         <Route path="/search" render={ () => (
           <SearchBooks 
@@ -63,5 +50,4 @@ class BooksApp extends Component {
     )
   }
 }
-
 export default BooksApp
